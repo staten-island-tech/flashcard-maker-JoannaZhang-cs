06c8b3770c71cb80fd94931bdbd0e2ccd08a8b34
JoannaZhang-cs <jzsunset10@gmail.com>
2025-04-27 19:57:51 -0400

    Create appCOMPLETE.py

---
+++ appCOMPLETE.py
@@ -0,0 +1,65 @@
+import json
+mode = input("What mode: ").lower() #teacher or student
+
+
+class flashcards:
+    def __init__(self, question, answer):
+        self.question = question
+        self.answer = answer
+
+    def display_info(self):
+        return f"{self.question} {self.answer}"
+        
+    def to_dict(self):
+        return {"question": self.question, "answer": self.answer}
+
+if mode == "teacher":
+    try:
+        with open("flash.json", "r") as file:
+            flash_data = json.load(file)
+    except FileNotFoundError:
+        flash_data = []
+    except (FileNotFoundError, json.decoder.JSONDecodeError):
+        flash_data = []
+    
+    question = input("Enter the question: ")
+    answer = input("Enter the answer: ")
+    new_card = flashcards(question, answer)
+    flash_data.append(new_card.to_dict())
+    
+    with open("flash.json", "w") as file:
+        json.dump(flash_data, file, indent=4)
+    #appending flashcard dictionary to list in json
+    print("flashcard added")
+    print(new_card.display_info()) 
+
+    print("Loaded flashcards:", flash_data)
+elif mode == "student":
+    try:
+        with open("flash.json", "r") as file:
+            flash_data = json.load(file)
+    except FileNotFoundError:
+        print("no flashcards found")
+        exit()
+    except (FileNotFoundError, json.decoder.JSONDecodeError):
+        print("no flashcards found")
+        exit()
+
+    z = "yes"
+    while z.lower() == "yes":
+        for card in flash_data:
+            print(card["question"])
+            y = input("What is the answer? ")
+
+            if y.strip().lower() == card["answer"].strip().lower():
+                print(f"Correct! The answer is: {card['answer']}")
+            else:
+                print(f"Wrong. The correct answer is: {card['answer']}")
+
+        z = input("Do you want to go through the flashcards again? (yes/no): ")
+
+    print("ok bye!:)")
+    
+
+else: 
+    print("Invalid mode. Please choose 'teacher' or 'student'.")
